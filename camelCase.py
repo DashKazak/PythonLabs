0e733763f8722eee905f2ad94e14b6908e5975e9
Dash <123qwe@gmail.com>
2022-02-04 15:20:31 -0600

    added banner message

--- camelCase.py
+++ camelCase.py
@@ -1,9 +1,20 @@
+from email import message
+
+
 def camelcase(sentence):
     title_case = sentence.title()
     upper_camel_cased = title_case.replace(' ','') #remove spaces
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """"Display program name"""
+    message = "awesome program!"
+    stars = "*"* len(message)
+    print(f'\n{stars}\n{message}\n{stars}')
+
+
 def main():
+    banner()
     sentence = input ('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
